ec396fbe6458ab1e3f5f7bab143392eb36b9c80c
Nina D <128380844+nina-dry@users.noreply.github.com>
2023-05-06 13:14:53 +0200

    ajout challenges

--- Neos_L2/challenge.php
+++ Neos_L2/challenge.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Challenges</title>
-    <link rel="stylesheet" type="text/css" href="styleChallenges.css">
+    <link rel="stylesheet" type="text/css" href="styleChallenge.css">
 </head>
 
 <?php
